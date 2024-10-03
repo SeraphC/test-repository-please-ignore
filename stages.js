30841ef6612ddb7a11640825d66bbb81c4620dd9
matthieu-deroo <matthieuderoo@gmail.com>
2024-10-03 13:51:37 +0200

    Update stages.js

--- stages.js
+++ stages.js
@@ -3,14 +3,10 @@ import { sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 50 },
-    { duration: '3m', target: 50 },
-    { duration: '1m', target: 0 },
+    { target: 10, duration: '30s' },
+    { target: 50, duration: '1m' },
+    { target: 10, duration: '30s' },
   ],
-  thresholds: {
-    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
-  },
 };
 
 export default function () {

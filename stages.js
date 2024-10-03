d217e7e6f37d2bdddd18579983700cfce0ad62a8
matthieu-deroo <matthieuderoo@gmail.com>
2024-10-03 13:45:20 +0200

    Create stages.js

---
+++ stages.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '30s', target: 50 },
+    { duration: '3m', target: 50 },
+    { duration: '1m', target: 0 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<500'], // 95 percent of response times must be below 500ms
+  },
+};
+
+export default function () {
+  const res = http.get('https://quickpizza.grafana.com/');
+  sleep(1);
+}
